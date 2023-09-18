16184ab7f433c6855e88a04cc809cd54536a305a
Iana Banina <janedragonfox@gmail.com>
2023-09-18 15:07:20 +0700

    Add provider for redux store

--- src/app/pages/App.tsx
+++ src/app/pages/App.tsx
@@ -1,8 +1,11 @@
+import {Provider} from "react-redux";
+import {store} from "app/store";
+
 function App() {
     return (
-        <>
+        <Provider store={store}>
             React messager
-        </>
+        </Provider>
     )
 }
 
