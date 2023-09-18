d1e8ef1ffd0bce654902187ba9a1777c22e6396e
Iana Banina <janedragonfox@gmail.com>
2023-09-18 17:54:29 +0700

    Add base card component

--- src/app/pages/main.tsx
+++ src/app/pages/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'app/assets/styles/index.css'
+import 'app/assets/styles/index.scss'
 import App from "app/pages/App.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
---
+++ src/common/components/Card.tsx
@@ -0,0 +1,42 @@
+import {Avatar} from "antd";
+
+interface IComponentProps {
+    description: string;
+    date: string;
+    header?: string;
+    isAvatarOnRight?: boolean;
+    avatarText?: string;
+    onAvatarClick?: () => void;
+    onHeaderClick?: () => void;
+}
+
+export function Card(props: IComponentProps) {
+    const {header, description, date, isAvatarOnRight, avatarText, onAvatarClick, onHeaderClick} = props;
+
+    return <div className={`card-wrapper ${isAvatarOnRight ? 'card-wrapper_right' : ''}`}>
+        <div className={`card ${isAvatarOnRight ? 'card_right' : ''}`}>
+            {avatarText && <div className={'card__avatar'}>
+                <Avatar className={` ${onAvatarClick ? 'c-pointer' : undefined}`}
+                        onClick={onAvatarClick}>
+                    {avatarText}
+                </Avatar>
+            </div>}
+
+            <div className={'card__content'}>
+                {header &&
+                    <div className={`card__header ${onHeaderClick ? 'c-pointer' : undefined}`} onClick={onHeaderClick}>
+                        {header}
+                    </div>}
+
+                <div className={'card__description'}>
+                    {description}
+                </div>
+
+                <div className={'card__date'}>
+                    {/*TODO: Format date*/}
+                    {date}
+                </div>
+            </div>
+        </div>
+    </div>
+}
