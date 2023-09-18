7e15d962e4466a225f4540e4ddd920ceec1fca37
Iana Banina <janedragonfox@gmail.com>
2023-09-18 15:06:51 +0700

    Use absolute paths instead of relative

--- src/app/pages/main.tsx
+++ src/app/pages/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import '../assets/styles/index.css'
+import 'app/assets/styles/index.css'
+import App from "app/pages/App.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
 )
